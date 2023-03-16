5f64bc644a699ff02bb37cbd57d6351eda3556f9
AsimChakraborty <asimcb18@gmail.com>
2023-03-16 15:03:14 +0600

    updated

--- Heart Disease/app.py
+++ Heart Disease/app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import numpy as np
 import pickle
-
+  
+  
 app = Flask(__name__)
+  
+  
+app.secret_key = 'xyzsdfg'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user'
+  
+mysql = MySQL(app)
+  
+
 ## Load the model
 try:
     with open('trained_model.sav', 'rb') as file:
@@ -15,9 +31,12 @@ else:
     # code that uses the model only if it was successfully loaded
     print(model) 
 
+
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
 
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -44,13 +63,72 @@ def predict():
         values = np.array([[Gender,age,currentSmoker,cigsPerDay,BPMeds,prevalentStroke,prevalentHyp,diabetes,totChol,sysBP,diaBP,BMI,heartRate,glucose]])
         prediction = model.predict(values)
         prediction = round(prediction[0],2)
+        if (prediction == 0):
+            prediction_text = 'You have no Heart Disease'
+        else:
+            prediction_text = 'You have Heart Disease'
+       
+        return render_template('dashboard.html', prediction_text=prediction_text)
+
+
+
+@app.route('/dashboard',methods=['GET','POST']) 
+
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return render_template('index.html', mesage = mesage)
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+  
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
     
-
-        return render_template('index.html', prediction_text='Heart Disease 0 means no Heart Disease. 1 is Heart Disease .Result is {}'.format(prediction))
-
-
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
